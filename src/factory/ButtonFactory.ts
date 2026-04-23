6ebb2dd03a3ab16eece597289e15daa152a2fbb9
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-04-23 21:33:31 +0200

    cleaning for linter

--- src/factory/ButtonFactory.ts
+++ src/factory/ButtonFactory.ts
@@ -13,6 +13,7 @@ type ButtonStatusValue = (typeof buttonStatus)[ButtonStatusKey];
 
 export default class ButtonFactory {
   public static button_vm: ButtonViewModel;
+
   private static bus: CustomEvents;
 
   static init(button_vm: ButtonViewModel, bus: CustomEvents): void {
@@ -29,32 +30,32 @@ export default class ButtonFactory {
     container: string,
     status: string,
   ) {
+    const bus = ButtonFactory.bus;
+    const button_vm = ButtonFactory.button_vm;
     const button = document.createElement("button");
     button.innerHTML = label;
     button.id = id;
     const s: ButtonStatusValue =
       buttonStatus[status as ButtonStatusKey] ?? "none";
     button.className = class_name + " " + s;
-    const B = ButtonFactory.button_vm.create_button(id);
-    ButtonFactory.button_vm.change_button_state(B.id, status);
+    const B = button_vm.create_button(id);
+    button_vm.change_button_state(B.id, status);
 
     document.getElementById(container)?.appendChild(button);
 
     if (trigger_action !== "") {
       button.addEventListener(trigger_action, (e) => {
         // cette partie bouton va être améliorée
-        ButtonFactory.bus.deactivate_all_actions();
+        bus.deactivate_all_actions();
         if ((e.target as HTMLElement)?.id !== undefined) {
-          ButtonFactory.button_vm.toggle_state_state(
-            (e.target as HTMLElement)?.id,
-          );
+          button_vm.toggle_state_state((e.target as HTMLElement)?.id);
         }
 
         if (B.state === "activated" || B.id === "add_node") {
-          ButtonFactory.bus.active_action_key = id;
+          bus.active_action_key = id;
           action(B);
         } else {
-          ButtonFactory.bus.active_action_key = null;
+          bus.active_action_key = null;
         }
       });
     }
