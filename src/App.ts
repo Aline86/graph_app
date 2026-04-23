3f7490af4866e4abfc872ff628c0ab65f16ea1c5
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-04-23 20:02:54 +0200

    clean centralized app

--- src/App.ts
+++ src/App.ts
@@ -7,47 +7,42 @@ import type Graph from "./model/Graph";
 import AlgorithmModule from "./modules/algorithms/AlgorithmModule";
 import GraphModule from "./modules/GraphModule";
 import StorageModule from "./modules/StorageModule";
-import DomUtils from "./utils/DomUtils";
 import NodeMenuView from "./view/NodeMenuView";
 import ButtonViewModel from "./viewmodel/ButtonViewModel";
 
-export default class App extends DomUtils {
+export default class App {
   static activeActionKey: string | null = null;
-  container: HTMLElement | null = null;
+  container: HTMLElement;
   graph: Graph;
   button_vm: ButtonViewModel;
-  node_menu_view: NodeMenuView;
-  button_db: ButtonDomBuilder;
+  node_menu_view: NodeMenuView | null;
+  button_db: ButtonDomBuilder | null;
 
   constructor(graph: Graph, container_name: string, bus: AlgorithmEvents) {
-    super();
-
-    this.container = document.createElement("div");
-    this.container.id = graph.id;
-    document.querySelector(container_name)?.appendChild(this.container);
-    const button_container = document.createElement("div");
-    button_container.id = "button_container";
-    this.container.appendChild(button_container);
-
+    this.node_menu_view = null;
+    this.button_db = null;
     this.graph = graph;
-
+    this.container = this.create_container(container_name);
     this.button_vm = new ButtonViewModel(this.graph, bus);
     ButtonFactory.init(this.button_vm, bus);
     //StorageModule.clear();
-    this.container.className = "graph";
 
     const storage_module = new StorageModule(this.graph);
-
     storage_module.install(bus);
-
     const graph_module = new GraphModule(this.graph);
-
     graph_module.install(bus, this.container);
 
     const algorithm_module = new AlgorithmModule(this.graph);
     algorithm_module.install(bus, this.container);
+
+    // call after modules so all button functionnalities are registered in core module
+    this.load_palette_and_node_menu(bus);
+  }
+
+  private load_palette_and_node_menu(bus: AlgorithmEvents) {
     this.button_db = new ButtonDomBuilder(this.button_vm, bus);
     this.node_menu_view = new NodeMenuView(this.graph, bus);
+
     const palette_db = new PaletteDomBuilder(this.container, bus, this.graph);
     new PaletteEventHandler(
       palette_db,
@@ -56,5 +51,17 @@ export default class App extends DomUtils {
       this.graph.nodes,
       bus,
     );
+  }
+
+  private create_container(id: string): HTMLElement {
+    const container = document.createElement("div");
+    container.id = this.graph.id;
+    document.querySelector(id)?.appendChild(container);
+    const button_container = document.createElement("div");
+    button_container.id = "button_container";
+    container.appendChild(button_container);
+    container.className = "graph";
+
+    return container;
   }
 }
